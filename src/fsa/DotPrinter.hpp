31078032a4812789003bb06c67e5e14987890db8
Alessandro Bertulli <berbu96@gmail.com>
2022-01-28 20:11:19 +0100

    Add license note for missing files

--- src/fsa/DotPrinter.hpp
+++ src/fsa/DotPrinter.hpp
@@ -1,6 +1,24 @@
 #ifndef DOTPRINTER_H
 #define DOTPRINTER_H
 
+/*************************************************************************/
+/* Copyright Alessandro Bertulli 2022                                    */
+/* This file is part of ExpLaineR1.					 */
+/* 									 */
+/* ExpLaineR1 is free software: you can redistribute it and/or modify it */
+/* under the terms of the GNU General Public License as published by	 */
+/* the Free Software Foundation, either version 3 of the License, or	 */
+/* (at your option) any later version.					 */
+/* 									 */
+/* ExpLaineR1 is distributed in the hope that it will be useful, but	 */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of		 */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU	 */
+/* General Public License for more details.				 */
+/* 									 */
+/* You should have received a copy of the GNU General Public License	 */
+/* along with ExpLaineR1. If not, see <https://www.gnu.org/licenses/>.	 */
+/*************************************************************************/
+
 #include "MachinePrinter.hpp"
 
 class DotPrinter : public MachinePrinter
--- src/fsa/LatexPrinter.cpp
+++ src/fsa/LatexPrinter.cpp
@@ -1,3 +1,21 @@
+/*************************************************************************/
+/* Copyright Alessandro Bertulli 2022                                    */
+/* This file is part of ExpLaineR1.					 */
+/* 									 */
+/* ExpLaineR1 is free software: you can redistribute it and/or modify it */
+/* under the terms of the GNU General Public License as published by	 */
+/* the Free Software Foundation, either version 3 of the License, or	 */
+/* (at your option) any later version.					 */
+/* 									 */
+/* ExpLaineR1 is distributed in the hope that it will be useful, but	 */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of		 */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU	 */
+/* General Public License for more details.				 */
+/* 									 */
+/* You should have received a copy of the GNU General Public License	 */
+/* along with ExpLaineR1. If not, see <https://www.gnu.org/licenses/>.	 */
+/*************************************************************************/
+
 #include "LatexPrinter.hpp"
 
 #include "Machine.hpp"
